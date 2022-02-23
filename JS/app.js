6b6cf2a56e16540d4b450ace481328f974d3ad69
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-02-23 13:44:43 +0600

    Make Random Friend

--- JS/app.js
+++ JS/app.js
@@ -10,3 +10,26 @@ const printQuote = quote => {
     document.getElementById('quote-box').value = quote.quote
 }
 
+/* =============== Generate Quote ^ =============== */
+
+
+/* =============== Make New Friend =============== */
+const friendName = document.getElementById('fnd-name')
+const friendLocation = document.getElementById('fnd-country')
+const friendImg = document.getElementById('friend-img')
+const makeFriend = () => {
+    const URL = 'https://randomuser.me/api/'
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => printFriend(data))
+}
+
+const printFriend = (friend) => {
+    const friendsss = friend.results[0]
+    const name = `${friendsss.name.title} ${friendsss.name.first} ${friendsss.name.last}`
+    friendName.innerText = name;
+    friendLocation.innerText = `${friendsss.location.country}`
+    friendImg.src = `${friendsss.picture.large}`
+    console.log(friend.results[0])
+}
+/* =============== Make New Friend ^ =============== */
